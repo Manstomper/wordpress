45bd9fcc9c7990323e830a38c3584e7f0dbb9bbc
Manstomper <example@nospam.com>
2022-08-11 09:38:15 +0300

    php8.1 and other updates

--- dev/init.sh
+++ dev/init.sh
@@ -2,7 +2,7 @@
 
 cp -n ./.env.local ./.env
 
-docker build . -f "$(pwd)/local/nginx/Dockerfile" -t rig/nginx:1.21.5 \
-    && docker build . -f "$(pwd)/local/php80/Dockerfile" -t rig/php:8.0.14 \
+docker build . -f "$(pwd)/local/nginx/Dockerfile" -t rig/nginx:1.23.1 \
+    && docker build . -f "$(pwd)/local/php80/Dockerfile" -t rig/php:8.1.9 \
     && docker-compose up -d \
     && docker-compose run --rm -v "$(pwd):/app-mount" -w /app-mount php sh -c "composer install --no-interaction"
