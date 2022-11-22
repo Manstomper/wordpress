8fe16b808450f92beea9695b40dfeb26cf9b34e9
Manstomper <nospam@nospam.com>
2022-11-22 14:39:56 +0200

    small changes

--- dev/wp-cli.sh
+++ dev/wp-cli.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker compose exec php sh -c 'cd /app-mount/public; printf "\nYou should now be inside the PHP container. You can run commands such as \"wp core version --allow-root\".\nType exit to quit.\n\n"; sh'
+docker compose exec php sh -c 'cd /app-mount/public; printf "\nYou should now be in the PHP container. You can run commands such as \"wp core version --allow-root\".\nType exit to quit.\n\n"; sh'
